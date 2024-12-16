eeac686aca2be40261a6e4834a8950bf9ba61588
Asad Ullah Shah <asadullahshah002@gmail.com>
2024-12-16 20:11:11 +0500

    V 1.0.1

--- src/ComingSoon.js
+++ src/ComingSoon.js
@@ -2,13 +2,14 @@ import React from "react";
 import "./App.css"; // Assuming you will add styles in App.css or use Tailwind CSS
 
 // Importing the SVG assets
-import BG from "./assets/BG.svg";
+import BG from "./assets/HomePage BG.svg";
 import Logo from "./assets/Logo.svg";
 import Bar from "./assets/Bar.svg";
 import SymbolLogo from "./assets/Symbol Logo.svg";
 import BlurGreenLine from "./assets/Blur Green Line.svg";
 import GreenDashUnderline from "./assets/Green Dash Underline.svg";
 import RORBG from "./assets/BG2.svg";
+import Bottom from "./assets/Bottom Image.svg";
 
 const ComingSoon = () => {
     return (
@@ -24,7 +25,7 @@ const ComingSoon = () => {
                 <img src={SymbolLogo} alt="Symbol" className="divider-bar-image2" />
             </div>
             {/* </div> */}
-            <div className="coming-soon-container" style={{ backgroundImage: `url(${RORBG})` }}>
+            <div className="coming-soon-container">
                 {/* Divider Bar */}
                 {/* <div className="divider-bar"> */}
                 {/* <div> */}
@@ -40,27 +41,35 @@ const ComingSoon = () => {
                 {/* </div> */}
 
                 {/* Coming Soon Section */}
-                <div className="content-section">
-                    <h1 className="coming-soon-title">COMING SOON!</h1>
-                    <img
-                        src={GreenDashUnderline}
-                        alt="Green Dash Underline"
-                        className="green-dash-underline"
-                    />
-                    <p className="content-text">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce euismod
-                        magna non nisl vulputate, eget tempor urna bibendum. Donec pulvinar
-                        risus ut orci fermentum hendrerit.
-                    </p>
-                </div>
 
                 {/* Blur Line */}
-                <div className="blur-line">
-                    <img src={BlurGreenLine} alt="Blur Green Line" className="blur-line-image" />
+                {/* <div className="divider-bar">
+                    <img src={Bar} alt="Divider Bar" class="divider-bar-image1 bottom" />
+                    <img src={SymbolLogo} alt="Symbol" class="divider-bar-image2 bottom" />
+                </div> */}
+                <div className="content-container">
+                    <img src={RORBG} alt="Background Top" className="bg-image" />
+                    <div className="content-section">
+                        <h1 className="coming-soon-title" style={{ fontFamily: 'BahnSchrift', fontWeight: 'normal', color: '#e0e5e3' }}>Coming Soon!</h1>
+                        <img
+                            src={GreenDashUnderline}
+                            alt="Green Dash Underline"
+                            className="green-dash-underline"
+                        />
+                        <p className="content-text" style={{ fontFamily: 'BahnSchrift', fontWeight: 'lighter', color: '#e0e5e3' }}>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce euismod
+                            magna non nisl vulputate, eget tempor urna bibendum. Donec pulvinar
+                            risus ut orci fermentum hendrerit.
+                        </p>
+                        <img src={BlurGreenLine} alt="Blur Green Line" className="blur-line-image" />
+                    </div>
                 </div>
                 <div className="divider-bar">
-                    <img src={Bar} alt="Divider Bar" class="divider-bar-image1" />
-                    <img src={SymbolLogo} alt="Symbol" class="divider-bar-image2" />
+                    <img src={Bar} alt="Divider Bar" className="divider-bar-image1 bottom" />
+                    <img src={SymbolLogo} alt="Symbol" className="divider-bar-image2 bottom" />
+                </div>
+                <div className="footer">
+                    <img src={Bottom} alt="Background Bottom" class='bg-image' />
                 </div>
             </div>
         </div>
